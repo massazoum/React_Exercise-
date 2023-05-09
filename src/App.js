773079c3389c22843e79cd54dddaca7d6256ff28
massa <zoumaniguimassa12@gmail.com>
2023-05-09 08:10:09 +0000

    App todo Updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import  TodoItem from './components/TodoItem.jsx'
+import TodosLogic from './components/TodosLogic.jsx';
+
+function App() {
+  return (
+    <div className="App">
+     <TodosLogic/>
+    </div>
+  );
+}
+
+export default App;
